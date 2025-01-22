2d9ef88175fcc809ac4a17a5fadf42f66452cc2d
Bobik58 <loshininc@gmail.com>
2025-01-22 14:13:55 +0700

    res

--- Server/Program.cs
+++ Server/Program.cs
@@ -54,7 +54,7 @@ class Program // сервер
             Console.WriteLine("Ответ сервера: " + response);
 
             // Отправляем ответ клиенту
-            byte[] responseBytes = Encoding.UTF8.GetBytes(response ?? "Продукт не найден");//
+            byte[] responseBytes = Encoding.UTF8.GetBytes(response ?? "Продукт не найден");
             networkStream.Write(responseBytes, 0, responseBytes.Length);
             Console.WriteLine("Ответ отправлен клиенту.");
         }
@@ -63,7 +63,6 @@ class Program // сервер
         client.Close();
         listener.Stop();
     }
-
     static async Task<string> SearchData(string productName)
     {
         string productInfo = null;
