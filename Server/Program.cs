444497e0130cf4fa4420a4a2cae5369c5ce9701f
Bobik59 <loshininc@gmail.com>
2025-01-23 17:21:51 +0700

    manyThings

--- Server/Program.cs
+++ Server/Program.cs
@@ -99,6 +99,31 @@ class Program // сервер
         client.Close();
         listener.Stop();
     }
+
+    public static void CreateFile(string path, string type, string response, double weight, double height, string gender)
+    {
+
+        try
+        {
+
+            FileInfo fileInfo = new FileInfo(path);
+            FileStream fs = fileInfo.Create();
+            if (type == "anketa")
+            {
+                fs.Write(Encoding.UTF8.GetBytes($"{weight}, {height}, {gender}"));
+            }
+            if (type == "product")
+            {
+                fs.Write(Encoding.UTF8.GetBytes(response));
+            }
+            fs.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
     static async Task<string> SearchData(string productName)
     {
         string productInfo = null;
